a913c67a2b56ece45158b336e53e65afe61575f0
Magggi6 <117828722+Magggi6@users.noreply.github.com>
2022-11-17 04:39:24 -0800

    Update proyecto_con_arduino.ino

--- proyecto_con_arduino.ino
+++ proyecto_con_arduino.ino
@@ -1,19 +1,4 @@
-#include <Servo.h>
-Servo motor;
-void setup() { 
- pinMode(4,OUTPUT); 
- pinMode(5,OUTPUT);
-motor.attach(3);
-}
-void loop() {  
-motor.write(random(160));
-delay(277);
-digitalWrite(4,HIGH);
-delay(277);
-digitalWrite(4,LOW);
-digitalWrite(5,HIGH);
-delay(277);
-digitalWrite(5,LOW);
-delay(277);
+# sistem< de riego
+este esun progrm pr hcer un istem de riego y ver l humedd del suelo
 
-}
+ 
